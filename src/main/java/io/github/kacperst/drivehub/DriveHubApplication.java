618c36b2eb5e5247ef3f37e35b1ed7117ae06023
Kacper <stefaniuk880@gmail.com>
2026-02-18 11:50:25 +0100

    initial commit

---
+++ src/main/java/io/github/kacperst/drivehub/DriveHubApplication.java
@@ -0,0 +1,13 @@
+package io.github.kacperst.drivehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriveHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/kacperst/drivehub/DriveHubApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kacperst.drivehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
